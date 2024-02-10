7b4ea9fcb0764851baa2507a8264be0bd8f2beac
alexander <alexander@hal>
2024-02-10 12:36:14 +0100

    Add tinyusb

--- firmware/src/usb_test/main.c
+++ firmware/src/usb_test/main.c
@@ -1,17 +1,11 @@
-#include <stdio.h>
-#include <math.h>
-#include <stdlib.h>
-#include "hardware/pio.h"
-#include "hardware/irq.h"
-#include "hardware/dma.h"
-#include "hardware/timer.h"
+#include "pico/stdlib.h"
 
 #define LED 25
 
 int main(int argc, char const *argv[]) {
 
     gpio_init(LED);
-    gpio_set_dir(LED, true);
+    gpio_set_dir(LED, 1);
 
     while (true) {
         sleep_ms(500);
